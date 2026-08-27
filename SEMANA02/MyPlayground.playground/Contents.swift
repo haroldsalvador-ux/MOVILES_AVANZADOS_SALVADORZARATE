1ac1d96ce87b462cf4756c11ea63622e950f0501
Harold David Salvador Zarate <harold.salvador@tecsup.edu.pe>
2026-08-27 13:21:20 -0500

    Commid de ingreso de datos

--- SEMANA02/MyPlayground.playground/Contents.swift
+++ SEMANA02/MyPlayground.playground/Contents.swift
@@ -1,12 +1,4 @@
-import Foundation
-// framework de swift para el uso de comandos
 
-print("Numero 1")
-let a = Double(readLine() ?? "") ?? 0
+import UIKit
 
-print("Numero 2")
-let b = Double(readLine() ?? "") ?? 0
-
-let suma = a + b
-print("La suma es \(suma)")
-
+var greeting = "Hello, playground"
