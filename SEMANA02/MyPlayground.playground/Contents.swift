6147159326d004b298b85d0d32dcf765daa2e102
Harold David Salvador Zarate <harold.salvador@tecsup.edu.pe>
2026-08-27 13:22:38 -0500

    Commit de ingreso de Datos

--- SEMANA02/MyPlayground.playground/Contents.swift
+++ SEMANA02/MyPlayground.playground/Contents.swift
@@ -1,4 +1,21 @@
-
 import UIKit
+import Foundation
 
 var greeting = "Hello, playground"
+// Definir el promedio por pesos
+
+let parcial = 0.3
+let trabajo = 0.3
+let final = 0.4
+
+print("Nombre del Alumno")
+let alumno = readLine() ?? "Desconocido"
+
+print ("Nota del Examen Parcial")
+let exaparcial = Double(readLine() ?? "") ?? 0
+
+print ("Nota del Examen de Trabajo")
+let exatrabajo = Double(readLine() ?? "") ?? 0
+
+print ("Nota del Examen Final")
+let exafinal = Double(readLine() ?? "") ?? 0
