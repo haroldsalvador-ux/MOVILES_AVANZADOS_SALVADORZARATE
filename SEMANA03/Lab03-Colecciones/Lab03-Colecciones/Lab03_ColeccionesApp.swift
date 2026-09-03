fa1c7bb2e886fc894308e7c6178fe4b516c8b22c
Harold Salvador Zarate <harold.salvador@tecsup.edu.pe>
2026-09-03 13:13:34 -0500

    feat: Crear proyecto Lab03 y Playground en Semana03

---
+++ SEMANA03/Lab03-Colecciones/Lab03-Colecciones/Lab03_ColeccionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab03_ColeccionesApp.swift
+//  Lab03-Colecciones
+//
+//  Created by Harold David Salvador Zarat on 3/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab03_ColeccionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
